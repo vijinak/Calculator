d73eef3cc745b3e06b05dceb2b793585cd5792ad
vijinak <vijinavijayankv@gmail.com>
2024-05-08 10:28:22 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,20 @@
+function display(num){
+    result.value += num
+}
+function clearAll(){
+    result.value = ''
+}
+function back(){
+    result.value = result.value.slice(0,-1)
+}
+function equal(){
+    try{
+        result.value = eval(result.value)
+    }
+    catch{
+        result.value = 'error'
+        setTimeout(() => {
+            result.value = ''
+        }, 2000);
+    }
+}
